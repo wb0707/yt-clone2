fab20c56ea10ca4484f0a211bc9942fb999b8342
SR Shihab <84579722+wb0707@users.noreply.github.com>
2021-08-11 12:52:21 +0600

    hgfthgn

---
+++ yt-clone/image/aa/content.js
@@ -0,0 +1,64 @@
+var Videos = [
+    {
+        vdo: "video",
+       channelImg: "./image/member1.jpg",
+       title:"hello everyone this me sr shihab i want to tell",
+       threeDotIcon:":",
+       channelName:"SR Shihab",
+       views:"1.4m views .",
+       uploadTime:"4 hours ago"
+    },
+    {
+         vdo: "video",
+        channelImg: "./image/member1.jpg",
+        title:"hello everyone this me sr shihab i want to tell",
+        threeDotIcon:":",
+        channelName:"SR Shihab",
+        views:"1.4m views .",
+        uploadTime:"4 hours ago"
+     },
+
+     {
+         vdo: "video",
+        channelImg: "./image/member1.jpg",
+        title:"hello everyone this me sr shihab i want to tell",
+        threeDotIcon:":",
+        channelName:"SR Shihab",
+        views:"1.4m views .",
+        uploadTime:"4 hours ago"
+     },
+
+     {
+         vdo: "video",
+        channelImg: "./image/member1.jpg",
+        title:"hello everyone this me sr shihab i want to tell",
+        threeDotIcon:":",
+        channelName:"SR Shihab",
+        views:"1.4m views .",
+        uploadTime:"4 hours ago"
+     },
+
+     {
+         vdo: "video",
+        channelImg: "./image/member1.jpg",
+        title:"hello everyone this me sr shihab i want to tell",
+        threeDotIcon:":",
+        channelName:"SR Shihab",
+        views:"1.4m views .",
+        uploadTime:"4 hours ago"
+     },
+
+     {
+         vdo: "video",
+        channelImg: "./image/member1.jpg",
+        title:"hello everyone this me sr shihab i want to tell",
+        threeDotIcon:":",
+        channelName:"SR Shihab",
+        views:"1.4m views .",
+        uploadTime:"4 hours ago"
+     },
+
+
+];
+
+export default Videos;
